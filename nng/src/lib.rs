3bf817205c1a5b12050dbd4f3d5a72351aab67d6
Nathan Kent <nkent2@cs.rochester.edu>
2018-10-30 17:35:41 -0400

    Move macros to separate module
    The root lib file was getting cluttered. This will be much easier to
    read.

--- nng/src/lib.rs
+++ nng/src/lib.rs
@@ -3,87 +3,8 @@ extern crate nng_sys;
 
 use std::time::Duration;
 
-/// Converts a `nng` return code into a Rust `Result`.
-macro_rules! rv2res
-{
-	($rv:expr, $ok:expr) => (
-		match $rv {
-			0 => Ok($ok),
-			e => Err(crate::error::ErrorKind::from_code(e).into()),
-		}
-	);
-
-	($rv:expr) => ( rv2res!($rv, ()) )
-}
-
-/// Checks an `nng` return code and validates the pointer.
-macro_rules! validate_ptr
-{
-	($rv:ident, $ptr:ident) => (
-		if $rv != 0 {
-			return Err(crate::error::ErrorKind::from_code($rv).into());
-		}
-		assert!($ptr != std::ptr::null_mut(), "Nng returned a null pointer from a successful function");
-	)
-}
-
-/// Utility macro for creating a new option type.
-///
-/// This is 90% me just playing around with macros. It is probably a terrible
-/// way to go around doing this task but, then again, this whole options
-/// business has been a complete mess.
-macro_rules! create_option
-{
-	(
-		$(#[$attr:meta])*
-		$opt:ident -> $ot:ty:
-		Get $g:ident = $gexpr:stmt;
-		Set $s:ident $v:ident = $sexpr:stmt;
-	) => {
-		$(#[$attr])*
-		pub enum $opt {}
-		impl $crate::options::private::Opt for $opt
-		{
-			type OptType = $ot;
-
-			fn get<T: $crate::options::private::HasOpts>($g: &T) -> $crate::error::Result<Self::OptType> { $gexpr }
-			fn set<T: $crate::options::private::HasOpts>($s: &T, $v: Self::OptType) -> $crate::error::Result<()> { $sexpr }
-		}
-	}
-}
-
-/// Implements the specified options for the type.
-macro_rules! expose_options
-{
-	(
-		$struct:ident :: $member:ident -> $handle:ty;
-		GETOPT_BOOL = $go_b:expr;
-		GETOPT_MS = $go_ms:expr;
-		GETOPT_SIZE = $go_sz:expr;
-		GETOPT_SOCKADDR = $go_sa:expr;
-
-		SETOPT_MS = $so_ms:expr;
-
-		Gets -> [$($getters:ident),*];
-		Sets -> [$($setters:ident),*];
-	) => {
-		impl $crate::options::private::HasOpts for $struct
-		{
-			type Handle = $handle;
-			fn handle(&self) -> Self::Handle { self.$member }
-
-			const GETOPT_BOOL: unsafe extern "C" fn(Self::Handle, *const std::os::raw::c_char, *mut bool) -> std::os::raw::c_int = $go_b;
-			const GETOPT_MS: unsafe extern "C" fn(Self::Handle, *const std::os::raw::c_char, *mut nng_sys::nng_duration) -> std::os::raw::c_int = $go_ms;
-			const GETOPT_SIZE: unsafe extern "C" fn(Self::Handle, *const std::os::raw::c_char, *mut usize) -> std::os::raw::c_int = $go_sz;
-			const GETOPT_SOCKADDR: unsafe extern "C" fn(Self::Handle, *const std::os::raw::c_char, *mut nng_sys::nng_sockaddr) -> std::os::raw::c_int = $go_sa;
-
-			const SETOPT_MS: unsafe extern "C" fn(Self::Handle, *const std::os::raw::c_char, nng_sys::nng_duration) -> std::os::raw::c_int = $so_ms;
-		}
-
-		$(impl $crate::options::GetOpt<$crate::options::$getters> for $struct {})*
-		$(impl $crate::options::SetOpt<$crate::options::$setters> for $struct {})*
-	}
-}
+#[macro_use]
+mod macros;
 
 mod error;
 pub use error::{Error, ErrorKind, Result};
