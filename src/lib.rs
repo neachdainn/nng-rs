d15bb822861d7e198b9d74a2590f79dc5921beff
Nathan Kent <nkent2@cs.rochester.edu>
2019-06-20 19:27:14 -0400

    Fix doctests

--- src/lib.rs
+++ src/lib.rs
@@ -52,14 +52,14 @@
 //!     // Set up the client and connect to the specified address
 //!     let mut client = Socket::new(Protocol::Req0)?;
 //!     # // Don't error if we hit here before the server does.
-//!     # client.set_nonblocking(true);
+//!     # client.dial_async(ADDRESS)?;
+//!     # if false {
 //!     client.dial(ADDRESS)?;
+//!     # }
 //!
 //!     // Send the request from the client to the server. In general, it will be
 //!     // better to directly use a `Message` to enable zero-copy, but that doesn't
 //!     // matter here.
-//!     # // Do block until the server is ready.
-//!     # client.set_nonblocking(false);
 //!     client.send("Ferris".as_bytes())?;
 //!
 //!     // Wait for the response from the server.
